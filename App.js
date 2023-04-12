f7216abc4bc0c0ab381feccd84d398261ecc172a
Joshua <joshuamayowa23@yahoo.com>
2023-04-12 14:26:31 +0100

    installing dependencies and setting up routes

--- App.js
+++ App.js
@@ -1,20 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Provider as PaperProvider } from 'react-native-paper';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Page1 from './Sections/Onboarding/page1';
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <PaperProvider>
+        <NavigationContainer>
+          <Stack.Screen name='On Boarding 1' options={{headerShown: false}} component={Page1}></Stack.Screen>
+        </NavigationContainer>
+      </PaperProvider>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ Sections/Onboarding/page1.js
@@ -0,0 +1,10 @@
+import { View, Text, Image, StyleSheet } from "react-native";
+import PrimaryButton from "../../components/UI/PrimaryButton";
+
+const Page1 = () => {
+  return (
+    <div>Page</div>
+  )
+}
+
+export default Page1;
