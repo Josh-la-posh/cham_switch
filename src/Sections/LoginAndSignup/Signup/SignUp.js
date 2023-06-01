b04dbdfd4eab70974407bf47737667440a82268a
danielmordi <danielmordi@gmail.com>
2023-06-01 21:42:11 +0100

    Radio sugnup button

--- src/Sections/LoginAndSignup/Signup/SignUp.js
+++ src/Sections/LoginAndSignup/Signup/SignUp.js
@@ -3,35 +3,73 @@ import { View, Text, StyleSheet } from "react-native";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 import Input from "../../../components/UI/input";
 import PrimaryButton from "../../../components/UI/PrimaryButton";
+import RadioButtonSelect from '../../../components/UI/radioButton';
 
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../../../config/firebase";
+import { ref, set } from "firebase/database";
+import { db } from "../../../config/firebase";
 
 const SignUp = ({ navigation }) => {
   const [firstname, setFirstname] = useState("");
   const [lastname, setLastname] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [conPass, setConPass] = useState("");
+  const [selectedValue, setSelectedValue] = useState("");
 
-  const handleSignUp = () => {
 
-    createUserWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        // Signed in
-        const user = userCredential.user;
-        console.log(user.email);
-        console.log(user.password);
-        user && navigation.navigate("Confirm Email");
+  const radioOptions = [
+    { label: 'Employee', value: 'employee' },
+    { label: 'Line Manager', value: 'lineManager' },
+    { label: 'Human Resource Manager', value: 'hr' },
+  ];
+
+  const handleValueChange = (value) => {
+    setSelectedValue(value);
+  };
+  
+  function create() {
+    set(ref(db, "users/" + firstname), {
+      firstname: firstname,
+      lastname: lastname,
+      email: email,
+      password: password,
+      selectedValue: selectedValue
+    })
+      .then(() => {
+        alert("data updated");
       })
       .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        console.log(error.message);
-        // ..
+        alert(error);
       });
+  }
 
-    // setEmail("");
-    // setPassword("");
+  const handleSignUp = () => {
+    if (password === conPass) {
+      createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+          const user = userCredential.user;
+          console.log(user.email);
+          console.log(user.password);
+          user && create();
+          user && navigation.navigate("Confirm Email");
+          setFirstname("");
+          setLastname("");
+          setEmail("");
+          setPassword("");
+          setConPass("");
+          setSelectedValue("");
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          console.log(error.message, errorCode, errorMessage);
+          // ..
+        });
+    } else {
+      alert("Password do not match");
+    }
   };
 
   return (
@@ -43,7 +81,7 @@ const SignUp = ({ navigation }) => {
               placeholder="Firstname"
               containerInput={styles.containerInput}
               value={firstname}
-              onChangeText={setFirstname}
+              onChangeText={(name) => setFirstname(name)}
             ></Input>
           </View>
           <View>
@@ -51,7 +89,7 @@ const SignUp = ({ navigation }) => {
               placeholder="Lastname"
               containerInput={styles.containerInput}
               value={lastname}
-              onChangeText={setLastname}
+              onChangeText={(name) => setLastname(name)}
             ></Input>
           </View>
           <View>
@@ -60,7 +98,7 @@ const SignUp = ({ navigation }) => {
               containerInput={styles.containerInput}
               keyboardType="email-address"
               value={email}
-              onChangeText={setEmail}
+              onChangeText={(email) => setEmail(email)}
             ></Input>
           </View>
           <View>
@@ -69,7 +107,7 @@ const SignUp = ({ navigation }) => {
               containerInput={styles.containerInput}
               keyboardType="password"
               value={password}
-              onChangeText={setPassword}
+              onChangeText={(pass) => setPassword(pass)}
             ></Input>
           </View>
           <View>
@@ -77,7 +115,16 @@ const SignUp = ({ navigation }) => {
               placeholder="Confirm password"
               containerInput={styles.containerInput}
               keyboardType="password"
+              value={conPass}
+              onChangeText={(pass) => setConPass(pass)}
             ></Input>
+          </View>
+          <View>
+            <RadioButtonSelect
+              radioOptions={radioOptions}
+              selectedValue={selectedValue}
+              onValueChange={handleValueChange}
+            />
           </View>
         </View>
         <View style={styles.btnContainer}>
--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,16 +1,29 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
   apiKey: "AIzaSyC4Qaoe9XJ2Jtar7-nsA2rO5pMRf8FdJXY",
+
   authDomain: "chams-elms.firebaseapp.com",
+
+  databaseURL: "https://chams-83c1b-default-rtdb.firebaseio.com",
+
   projectId: "chams-elms",
+
   storageBucket: "chams-elms.appspot.com",
+
   messagingSenderId: "554197890570",
+
   appId: "1:554197890570:web:67e547550516a2313b11f3",
+
   measurementId: "G-ZDH5F2FSVZ"
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
+
+// initialize database
+
+export const db = getDatabase(app);
